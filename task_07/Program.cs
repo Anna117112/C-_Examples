1fa0bf03f2affbd9b0ae6edd93826f0a8afcc720
Anna <a.dyakonu85@mail.ru>
2022-11-22 00:29:43 +0300

    task_07

---
+++ task_07/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int RandomDidit()
+{
+    System.Console.WriteLine("Введите число ");
+    int digit = Convert.ToInt32(Console.ReadLine());
+    return digit;
+}
+
+void DayOffWeek(int dayWeek)
+{
+
+    if ((dayWeek == 6) || (dayWeek == 7))
+    {
+        System.Console.WriteLine($"{dayWeek} да");
+    }
+    else
+    {
+        System.Console.WriteLine($"{dayWeek} нет");
+    }
+}
+
+int number = RandomDidit();
+DayOffWeek(number);
