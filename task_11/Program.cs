a60f7d50aa53c266655cbff8827af95c2874aee9
Anna <a.dyakonu85@mail.ru>
2022-11-24 22:22:47 +0300

    task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход 
+число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int Result(int num)
+
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+
+    }
+    System.Console.WriteLine(count);
+    return count;
+}
+Result(255);
