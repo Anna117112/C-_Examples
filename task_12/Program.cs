00c08400ddd5714b67c319adc4860ae1bd70d4a9
Anna <a.dyakonu85@mail.ru>
2022-11-24 22:23:00 +0300

    task_12

---
+++ task_12/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+
+int Result(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+
+        result = result * i;
+
+    }
+    Console.WriteLine(result);
+    return result;
+
+}
+Result(4);
