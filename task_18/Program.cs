a1992e6b53adbdb47264d549811901b591dbe65c
Anna <a.dyakonu85@mail.ru>
2022-11-24 23:30:26 +0300

    task_18_1

--- task_18/Program.cs
+++ task_18/Program.cs
@@ -1,7 +1,59 @@
 ﻿/*
-Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную 
+степень B.
 
 3, 5 -> 243 (3⁵)
 
 2, 4 -> 16
 */
+
+int getNumberFromUser(string message)
+{
+
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void degreeOfPositive(int a, int b)
+{
+
+    int result = a;
+
+    while (b - 1 > 0)
+    {
+        result *= a;
+        b--;
+
+    }
+    System.Console.WriteLine(result);
+}
+
+void degreeOfNegative(int a, int b)
+{
+
+    double result = a;
+
+    while (b - 1 < 0)
+    {
+        result /= a;
+        b++;
+
+    }
+    System.Console.WriteLine(result);
+}
+
+void result(int a, int b)
+{
+    if (b <= 0)
+    {
+        degreeOfNegative(a, b);
+    }
+    else
+    {
+        degreeOfPositive(a, b);
+    }
+}
+int number1 = getNumberFromUser("Введите первое число");
+int number2 = getNumberFromUser("Введите второе число");
+result(number1, number2);
