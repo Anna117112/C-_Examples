b83c49798030905d00bf6e7bce57f863d9c2aad8
Anna <a.dyakonu85@mail.ru>
2022-11-30 17:03:32 +0300

    task_47

---
+++ task_50/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+[0][1][2][3]
+1 4 7 2
+[4][5][6][7]
+5 9 2 3
+[8][9][10][11]
+8 4 2 4
+
+9 -> 4
+5 -> 9
+
+*/
+
+int[,] generate2DArray(int lengthRow, int lengthCol)
+
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(1, 10 + 1);
+        }
+    }
+    return array;
+}
+
+
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2dArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] array, int pos)
+{
+    int result = 0;
+    int count = 0;
+    long arrayNumbers = (array.GetLongLength(0) * array.GetLongLength(1)) - 1;
+
+    if (pos > arrayNumbers)
+    {
+        System.Console.WriteLine($" {pos} - > такого числа в массиве нет");
+    }
+    else
+    {
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (pos > count)
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (pos == (i + j))
+                    {
+                        result = array[i, j];
+                    }
+                    count++;
+                }
+
+        }
+        System.Console.WriteLine($" {pos} - > {result} ");
+    }
+
+
+}
+
+
+int[,] array = generate2DArray(3, 4);
+
+FindNumber(array, 12);
+print2dArray(array);
---
+++ task_52/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+*/
+
+int[,] generate2DArray(int lengthRow, int lengthCol)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(0, 2 + 1);
+        }
+    }
+    return array;
+}
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2dArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SummCols(int[,] array)
+{
+    int summRols = 0;
+    int summCols = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summRols += array[i, j] / array.GetLength(1);
+            summCols += array[j, i] / array.GetLength(0);
+
+        }
+        System.Console.Write($"{summRols};{summCols}");
+        System.Console.Write(" ");
+        summRols = 0;
+        summCols = 0;
+        //int summCols = 0;
+    }
+
+
+
+
+}
+
+int[,] array = generate2DArray(2, 2);
+print2dArray(array);
+SummCols(array);
